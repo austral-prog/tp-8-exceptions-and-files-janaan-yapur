cfe8c7b6355705834e718329e47cb6be92b49c9f
Janaan-Yapur <jyapur@mail.austral.edu.ar>
2026-05-18 21:46:00 -0300

    Update exercise_09_merge_files.py

--- exercise_09_merge_files.py
+++ exercise_09_merge_files.py
@@ -32,4 +32,12 @@ def merge_files(file1, file2, output):
         # hola
         # mundo
     """
-    pass  # Reemplazar con tu implementación
+    with open(file1, 'r') as archivo1:
+        contenido1 = archivo1.read()
+
+    with open(file2, 'r') as archivo2:
+        contenido2 = archivo2.read()
+
+    with open(output, 'w') as archivo3:
+        archivo3.write(contenido1)
+        archivo3.write(contenido2)
